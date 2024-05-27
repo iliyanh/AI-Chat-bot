938e69afb254ea7c5c09c3819d913e100d3eb7b8
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2024-05-27 11:34:27 +0100

    Final commit

--- talkbuddy/src/pages/index.js
+++ talkbuddy/src/pages/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 import { Inter } from 'next/font/google'
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -51,47 +52,52 @@ export default function Home() {
 
 
   return (
-    <div className="container">
-      {/* <!-- Chatbox --> */}
-      <div className="chatbox">
-        {/* <!-- Top Section --> */}
-        <div className="top-chatbox-section">
-          <div className="name logo">
-            <span>TalkBuddy</span>
-            <i className="fa-solid fa-robot"></i>
+    <>
+      <Head>
+        <title>TalkBuddy</title>
+        <link rel='icon' href='/robot-solid.svg' />
+      </Head>
+      <div className="container">
+        {/* <!-- Chatbox --> */}
+        <div className="chatbox">
+          {/* <!-- Top Section --> */}
+          <div className="top-chatbox-section">
+            <div className="name logo">
+              <span>TalkBuddy</span>
+              <i className="fa-solid fa-robot"></i>
+            </div>
           </div>
+          {/* <!-- Chat --> */}
+          <div className="chat-section" ref={chatSectionRef}>
+            {chatHistory.map((message, index) => {
+              return (
+                <div
+                  key={index}
+                  className={message.type === "user" ? "client-chat" : "bot-chat"}
+                >
+                  {message.message}
+                </div>
+              );
+            })}
+            {
+              isLoading &&
+              <div className="bot-chat">Thinking...</div>
+            }
+          </div>
+          {/* <!-- Input Section --> */}
+          <form className="input-section" onSubmit={submitHandler}>
+            <input
+              type="text"
+              placeholder="Message TalkBuddy"
+              value={inputMessage}
+              onChange={(e) => setInputMessage(e.currentTarget.value)}
+            />
+            <button type="submit">
+              <i className="fa-solid fa-circle-arrow-right"></i>
+            </button>
+          </form>
         </div>
-        {/* <!-- Chat --> */}
-        <div className="chat-section" ref={chatSectionRef}>
-          {chatHistory.map((message, index) => {
-            return (
-              <div
-                key={index}
-                className={message.type === "user" ? "client-chat" : "bot-chat"}
-              >
-                {message.message}
-              </div>
-            );
-          })}
-          {
-            isLoading &&
-            <div className="bot-chat">Thinking...</div>
-          }
-        </div>
-        {/* <!-- Input Section --> */}
-        <form className="input-section" onSubmit={submitHandler}>
-          <input
-            type="text"
-            placeholder="Message TalkBuddy"
-            value={inputMessage}
-            onChange={(e) => setInputMessage(e.currentTarget.value)}
-          />
-          <button type="submit">
-            <i className="fa-solid fa-circle-arrow-right"></i>
-          </button>
-        </form>
       </div>
-    </div>
-  );
+    </>);
 }
 
